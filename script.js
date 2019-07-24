fe20c21761f8b56c201edd4b2a705cc60c7862b7
John Sumner <sumnercommajohn@gmail.com>
2019-07-24 19:28:11 -0400

    add github project link and clean up script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const keys = Array.from(document.querySelectorAll('.key'));
-const star = document.querySelector('img.star');
+const starImage = document.querySelector('img.star');
 const instructions = document.querySelector('.instructions');
 const blast = document.querySelector('audio.blast');
 
@@ -7,27 +7,21 @@ let order = 1;
 let lastPlayed = 0;
 let secondLastPlayed = 0;
 
-
 function removeTransition(e) {
-
-    if (e.propertyName != 'transform') return;
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-
 }
 
 function animateStar(star) {
-
     star.classList.remove('offscreen');
-    setTimeout(()=> {
+    setTimeout(() => {
         star.classList.add('slideout');
-    },2000)
-    setTimeout(()=> {
-        star.classList.add('offscreen')
+    }, 2500);
+    setTimeout(() => {
+        star.classList.add('offscreen');
         star.classList.remove('slideout');
-    },2500)
-
+    }, 3000);
 }
-
 
 function playVoice(e) {
     instructions.style.opacity = 0;
@@ -39,26 +33,22 @@ function playVoice(e) {
 
     const key = document.querySelector(`div.key[data-key="${e.keyCode}"]`) || this;
     if (key === window) return;
-    
     const soundGroup = document.querySelector(`div.group[data-group="${key.dataset.group}"]`);
     const sound = soundGroup.querySelector(`audio[data-order="${order}"]`);
     key.classList.add('playing');
     sound.play();
     sound.currentTime = 0;
-
     if (order >= 8) {
-        animateStar(star);
+        animateStar(starImage);
         blast.play();
         order = 1;
     } else {
         order += 1;
     }
-    
+
     secondLastPlayed = lastPlayed;
     lastPlayed = sound;
-
 }
-
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 keys.forEach(key => key.addEventListener('click', playVoice));
